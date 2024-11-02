f05577ddef2d1b86509f8cc4736c87b1ec3d4b4e
preetianure <newpreeti15@gmail.com>
2024-11-02 19:06:25 +0530

    spotifymusic

--- practice.js
+++ practice.js
@@ -1,34 +1,6 @@
-// callback
+let div=document.querySelector("div");
+console.log(div);
 
-// let arr=[1,2,3,4,5];
-// // arr.forEach(function printval(val) {//value at each index
-// //     console.log(val);
-    
-// // })
-// // arrow function
-// arr.forEach((val)=>{
-//     console.log(val);
-    
-// })
-// //for uppercase
-// console.log(val.toUpperCase());
-// let arr=["pune","mumbai","hyderabad","bangalore"];
-// arr.forEach((val,idx,arr)=>{
-//     console.log(val.toUpperCase(),idx,arr);
-    
-// })
-// let nums=[2,3,4,5,6];
-// let calcsquare=(num)=>{
-//     console.log(num*num);
-// }
-// nums.forEach(calcsquare);
-// let arr=[1,2,3,4,5];
-// let evenArr=arr.filter((val)=>{
-//     return val%2===0;
-// })
-// console.log(evenArr);
-let arr=[1,2,3];
-arr.reduce((res,cur)=>{
-    return res+cur;
-});
-console.log(arr);
+let id=div.getAttribute("id");
+console.log(id);
+
---
+++ spotify.js
@@ -0,0 +1,3 @@
+document.getElementById('signupBtn').addEventListener('click', function() {
+    alert('Sign up functionality not implemented yet!');
+});
